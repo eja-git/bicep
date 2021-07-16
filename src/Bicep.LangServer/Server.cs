ea94f1bf6c66a872e3d20269b6406eeaa62a8d98
Marcin Jastrzebski <majastrz@microsoft.com>
2021-07-15 19:50:36 -0700

    fixed lsp di

--- src/Bicep.LangServer/Server.cs
+++ src/Bicep.LangServer/Server.cs
@@ -93,6 +93,7 @@ namespace Bicep.LanguageServer
             services.AddSingleton<IResourceTypeProvider>(services => creationOptions.ResourceTypeProvider ?? AzResourceTypeProvider.CreateWithAzTypes());
             services.AddSingleton<ISnippetsProvider>(services => creationOptions.SnippetsProvider ?? new SnippetsProvider(fileResolver));
             services.AddSingleton<IFileResolver>(services => fileResolver);
+            services.AddSingleton<IModuleRegistryProvider, DefaultModuleRegistryProvider>();
             services.AddSingleton<IModuleRegistryDispatcher, ModuleRegistryDispatcher>();
             services.AddSingleton<ITelemetryProvider, TelemetryProvider>();
             services.AddSingleton<IWorkspace, Workspace>();
